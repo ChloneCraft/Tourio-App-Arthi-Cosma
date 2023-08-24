ea6ba1aa427458582971711373f0089651d92f07
arthisolai <137558405+arthisolai@users.noreply.github.com>
2023-08-24 14:21:22 +0200

    create-record done

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -7,29 +7,18 @@ export default async function handler(request, response) {
   if (request.method === "GET") {
     const places = await Places.find();
     return response.status(200).json(places);
-  } else {
-    return response.status(405).json({ message: "Method not allowed" });
   }
+
+  if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      const place = new Places(placeData);
+      await place.save();
+      return response.status(201).json({ status: "Place created." });
+    } catch (error) {
+      console.error(error);
+      return response.status(400).json({ error: error.message });
+    }
+  }
+  return response.status(405).json({ message: "Method not allowed" });
 }
-//   await dbConnect();
-//   if (request.method === "GET") {
-//     console.log("Places in index", Places);
-//     const places = await Places.find();
-//     return response.status(200).json(places);
-//   } else {
-//     console.error("error:", error);
-//     return response.status(400).json({ error: error.message });
-//   }
-// }
-//   if (request.method === "POST") {
-//     try {
-//       const placeData = request.body;
-//       const place = new Places(placeData);
-//       await place.save();
-//       return response.status(201).json({ status: "Place created." });
-//     } catch (error) {
-//       console.error(error);
-//       return response.status(400).json({ error: error.message });
-//     }
-//   }
-// }
