fcf987cf312521bbb2505c975417bc3d8944292f
ChloneCraft <110734347+ChloneCraft@users.noreply.github.com>
2023-08-24 12:10:45 +0200

    creat-added

--- pages/create.js
+++ pages/create.js
@@ -3,30 +3,15 @@ import styled from "styled-components";
 import { useRouter } from "next/router";
 import Form from "../components/Form.js";
 import { StyledLink } from "../components/StyledLink.js";
-<<<<<<< HEAD
-import useSWR from "swr";
-=======
->>>>>>> 8a95e11e3163d08b0ed4e1f1c6f7764b5ccfe624
 
 const StyledBackLink = styled(StyledLink)`
   justify-self: flex-start;
 `;
 
 export default function CreatePlacePage() {
-  // const places = useSWR("/api/places");
   const router = useRouter();
 
   async function addPlace(place) {
-<<<<<<< HEAD
-    // const response = await fetch("/api/places", {
-    //   method: "POST",
-    //   body: JSON.stringify(place),
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    // });
-    console.log();
-=======
     console.log("Place added (but not really...)");
     const response = await fetch("/api/places", {
       method: "POST",
@@ -42,7 +27,6 @@ export default function CreatePlacePage() {
     } else {
       console.error(response.status);
     }
->>>>>>> 8a95e11e3163d08b0ed4e1f1c6f7764b5ccfe624
   }
 
   return (
