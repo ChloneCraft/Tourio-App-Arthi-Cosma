7f2cbb8b7788386f30fdec28b6207d59491dce55
ChloneCraft <110734347+ChloneCraft@users.noreply.github.com>
2023-08-24 14:39:24 +0200

    edit works

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,10 +1,10 @@
 import Places from "../../../../db/models/Places";
 import dbConnect from "../../../../db/connect";
+import { useRouter } from "next/router";
 
 export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
-  await dbConnect();
 
   if (!id) {
     return;
@@ -13,6 +13,11 @@ export default async function handler(request, response) {
   if (request.method === "GET") {
     const places = await Places.findById(id);
     return response.status(200).json(places);
+  } else if (request.method === "PATCH") {
+    await Places.findByIdAndUpdate(id, {
+      $set: request.body,
+    });
+    return response.status(200).json("updated successfully");
   } else {
     return response.status(405).json({ message: "Method not allowed" });
   }
--- pages/places/[id]/edit.js
+++ pages/places/[id]/edit.js
@@ -1,17 +1,24 @@
-import { useRouter } from 'next/router';
-import Link from 'next/link';
-import useSWR from 'swr';
-import Form from '../../../components/Form.js';
-import { StyledLink } from '../../../components/StyledLink.js';
+import { useRouter } from "next/router";
+import Link from "next/link";
+import useSWR from "swr";
+import Form from "../../../components/Form.js";
+import { StyledLink } from "../../../components/StyledLink.js";
 
 export default function EditPage() {
   const router = useRouter();
-  const { isReady } = router;
+  const { isReady, push } = router;
   const { id } = router.query;
   const { data: place, isLoading, error } = useSWR(`/api/places/${id}`);
 
   async function editPlace(place) {
-    console.log('Place edited (but not really...');
+    const response = await fetch(`/api/places/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify(place),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    push("/");
   }
 
   if (!isReady || isLoading || error) return <h2>Loading...</h2>;
@@ -22,7 +29,7 @@ export default function EditPage() {
       <Link href={`/places/${id}`} passHref legacyBehavior>
         <StyledLink justifySelf="start">back</StyledLink>
       </Link>
-      <Form onSubmit={editPlace} formName={'edit-place'} defaultData={place} />
+      <Form onSubmit={editPlace} formName={"edit-place"} defaultData={place} />
     </>
   );
 }
