b7c48570d6ca5daa237fb098f56f02294e1adfd4
ChloneCraft <110734347+ChloneCraft@users.noreply.github.com>
2023-08-24 12:04:18 +0200

    forgot branch

--- pages/create.js
+++ pages/create.js
@@ -1,18 +1,27 @@
-import Link from 'next/link.js';
-import styled from 'styled-components';
-import { useRouter } from 'next/router';
-import Form from '../components/Form.js';
-import { StyledLink } from '../components/StyledLink.js';
+import Link from "next/link.js";
+import styled from "styled-components";
+import { useRouter } from "next/router";
+import Form from "../components/Form.js";
+import { StyledLink } from "../components/StyledLink.js";
+import useSWR from "swr";
 
 const StyledBackLink = styled(StyledLink)`
   justify-self: flex-start;
 `;
 
 export default function CreatePlacePage() {
+  // const places = useSWR("/api/places");
   const router = useRouter();
 
-  function addPlace(place) {
-    console.log('Place added (but not really...)');
+  async function addPlace(place) {
+    // const response = await fetch("/api/places", {
+    //   method: "POST",
+    //   body: JSON.stringify(place),
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // });
+    console.log();
   }
 
   return (
@@ -21,7 +30,7 @@ export default function CreatePlacePage() {
       <Link href="/" passHref legacyBehavior>
         <StyledBackLink>back</StyledBackLink>
       </Link>
-      <Form onSubmit={addPlace} formName={'add-place'} />
+      <Form onSubmit={addPlace} formName={"add-place"} />
     </>
   );
 }
